45b35952696338f23b5ab6824e193a8b0385b9b9
ceepvoice <53182786+ceepvoice@users.noreply.github.com>
2019-08-14 08:00:04 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -7,16 +7,10 @@ function clientTime()
     var today = new Date();
 
     // Giờ, phút, giây hiện tại
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-
-    // Chuyển đổi sang dạng 01, 02, 03
-    m = checkTime(m);
-    s = checkTime(s);
+     var t = today.toLocaleTimeString();
 
     // Ghi ra trình duyệt
-    document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+    document.getElementById('time').innerHTML = t;
 
     // Dùng hàm setTimeout để thiết lập gọi lại 0.5 giây / lần
     var t = setTimeout(function() {
@@ -35,20 +29,43 @@ function checkTime(i)
 
 // KIỂM TRA TỐC ĐỘ INTERNET
 
+	
+
 online = window.navigator.onLine;
+
 navigator.connection.addEventListener('change', internetInfo);
 function internetInfo() {
   // Network type that browser uses
   document.getElementById('internet_type').innerHTML = navigator.connection.type;
-  document.getElementById('internet_downlink').innerHTML=navigator.connection.downlink + 'Mb/s';
-  document.getElementById('internet_rtt').innerHTML=navigator.connection.rtt + 'ms ';
-  document.getElementById('internet_downlinkMax').innerHTML=navigator.connection.downlinkMax + 'Mb/s ';
+  document.getElementById('internet_downlinkMax').innerHTML=navigator.connection.downlinkMax + 'Mb/s';
   document.getElementById('internet_effectiveType').innerHTML=navigator.connection.effectiveType;
   document.getElementById('internet_saveData').innerHTML= 'SD :'+navigator.connection.saveData;
+  // Vàng và nhấp nháy vàng nếu kết nối dưới 0.5 Mb và xanh nếu kết nối > 1 Mb 
+  if (navigator.connection.downlink <= 0.5) {
+  	document.getElementById('internet_downlink').innerHTML= '<yellow>'+navigator.connection.downlink + 'Mb/s</yellow>';
+ 	$("#internet_downlink").addClass("warningFlash");
+} else if ( navigator.connection.downlink >= 1 ) {
+  	document.getElementById('internet_downlink').innerHTML= '<green>'+navigator.connection.downlink + 'Mb/s</green>';
+  	$("#internet_downlink").removeClass("warningFlash");
+} else {
+	document.getElementById('internet_downlink').innerHTML= navigator.connection.rtt;
+	$("#internet_downlink").removeClass("warningFlash");
+};
+  // Đỏ và nhấp nháy nếu rtt dưới 100ms 
+  if (navigator.connection.rtt >= 100) {
+  	document.getElementById('internet_rtt').innerHTML= '<red>'+navigator.connection.rtt + 'ms</red>';
+ 	$("#internet_rtt").addClass("warningFlash");
+} else {
+  	document.getElementById('internet_rtt').innerHTML= navigator.connection.rtt + 'ms';
+  	$("#internet_rtt").removeClass("warningFlash");
+};
+  // Đỏ và nhấp nháy biểu tượng internet nếu mất kết nối
   if (navigator.onLine) {
   	document.getElementById('internet_connect').style.color = 'green';
+ 	$("#internet_connect").removeClass("warningFlash");
 } else {
-  	document.getElementById('internet_connect').style.color = 'red'
+  	document.getElementById('internet_connect').style.color = 'red';
+  	$("#internet_connect").addClass("warningFlash");
 }
-  
-}
+};
+	
